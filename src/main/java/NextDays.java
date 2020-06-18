ea9499bf292c788faf8b8ad23716066fe44bb787
QUYNH-TAM <ngocquynh.ngoctam@gmail.com>
2020-06-18 17:15:19 +0700

    [Bài tập] Tính ngày tiếp theo

--- src/main/java/NextDays.java
+++ src/main/java/NextDays.java
@@ -1,2 +1,99 @@
 public class NextDays {
+    public static boolean isLeapYear(int year) {
+        boolean result = false;
+        boolean isDivisibleBy4 = year % 4 == 0;
+        if (isDivisibleBy4) {
+            boolean isDivisibleBy100 = year % 100 == 0;
+            if (isDivisibleBy100) {
+                boolean isDivisibleBy400 = year % 400 == 0;
+                if (isDivisibleBy400) {
+                    result = true;
+                }
+            } else
+                result = true;
+        }
+        return result;
+    }
+
+    public static String nextDay(int day, int month, int year) {
+        int currentDay = 0;
+        int currentMonth = 0;
+        int currentYear = 0;
+
+        switch (month) {
+            case 1:
+            case 3:
+            case 7:
+            case 8:
+            case 10:
+                if (day < 31 && day > 0) {
+                    currentDay = day + 1;
+                    currentMonth = month;
+                    currentYear = year;
+                } else if (day == 31) {
+                    currentDay = 1;
+                    currentMonth = month + 1;
+                    currentYear = year;
+                } else
+                    System.out.println("Ngay Khong Hop Le");
+                break;
+            case 2:
+                if (isLeapYear(year)) {
+                    if (day < 29 && day > 0) {
+                        currentDay = day + 1;
+                        currentMonth = month;
+                        currentYear = year;
+                    } else if (day == 29) {
+                        currentDay = 1;
+                        currentMonth = month + 1;
+                        currentYear = year;
+                    } else
+                        System.out.println("Ngay Khong Hop Le");
+                } else {
+                    if (day < 28 && day > 0) {
+                        currentDay = day + 1;
+                        currentMonth = month;
+                        currentYear = year;
+                    } else if (day == 28) {
+                        currentDay = 1;
+                        currentMonth = month + 1;
+                        currentYear = year;
+                    } else
+                        System.out.println("Ngay Khong Hop Le");
+                }
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                if (day < 30 && day > 0) {
+                    currentDay = day + 1;
+                    currentMonth = month;
+                    currentYear = year;
+                } else if (day == 30) {
+                    currentDay = 1;
+                    currentMonth = month + 1;
+                    currentYear = year;
+                } else
+                    System.out.println("Ngay Khong Hop Le");
+                break;
+            case 12:
+                if (day < 31 && day > 0) {
+                    currentDay = day + 1;
+                    currentMonth = month;
+                    currentYear = year;
+                } else if (day == 31) {
+                    currentDay = 1;
+                    currentMonth = 1;
+                    currentYear = year + 1;
+                } else
+                    System.out.println("Ngay Khong Hop Le");
+                break;
+        }
+        return currentDay + " " + currentMonth + " " + currentYear;
+    }
+
+//    public static void main(String[] args) {
+//        System.out.println(nextDay(29, 2, 2020));
+//    }
 }
--- src/test/java/NextDaysTest.java
+++ src/test/java/NextDaysTest.java
@@ -1,24 +1,25 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class NextDaysTest {
-    public boolean isLeapYear(int year) {
-        boolean result = false;
-        boolean isDivisibleBy4 = year % 4 == 0;
-        if (isDivisibleBy4) {
-            boolean isDivisibleBy100 = year % 100 == 0;
-            if (isDivisibleBy100) {
-                boolean isDivisibleBy400 = year % 400 == 0;
-                if (isDivisibleBy400) {
-                    result = true;
-                }
-            } else
-                result = true;
-        }
-        return result;
+    @Test
+    void testAdd0And0() {
+        int first = 29;
+        int second = 2;
+        int third = 2020;
+        String expected = "1 3 2020";
+        String result = NextDays.nextDay(first, second, third);
+        assertEquals(expected, result);
     }
-
-    public int nextDay(int day, int month, int year) {
-
-        return 0;
+    @Test
+    void testAdd0And1() {
+        int first = 31;
+        int second = 12;
+        int third = 2018;
+        String expected = "1 1 2019";
+        String result = NextDays.nextDay(first, second, third);
+        assertEquals(expected, result);
     }
 }
